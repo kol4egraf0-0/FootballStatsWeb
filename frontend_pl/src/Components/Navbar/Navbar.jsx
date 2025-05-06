c6ce2426326b18526093647569054a8394f90c13
kol4egraf0-0 <genitya408@gmail.com>
2025-05-06 23:13:46 +0300

    add navbar

--- frontend_pl/src/Components/Navbar/Navbar.jsx
+++ frontend_pl/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,16 @@ import './Navbar.css'
 
 const Navbar = () =>{
     return(
-        <div>Navbar</div>
+       <header className="header">
+        <a href="/" className="logo">РПЛ Статистика игроков 23/24</a>
+
+        <nav className="navbar">
+            <a href="/">Главная</a>
+            <a href="/">Игроки</a>
+            <a href="/">Команды</a>
+            <a href="/">Позиции</a>
+        </nav>
+       </header>
     )
 }
 
