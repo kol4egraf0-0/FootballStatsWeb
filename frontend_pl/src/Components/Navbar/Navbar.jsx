af0a41e054bc9d37026870b9caa5c80ec35e898b
kol4egraf0-0 <genitya408@gmail.com>
2025-05-07 20:48:02 +0300

    fix route

--- frontend_pl/src/Components/Navbar/Navbar.jsx
+++ frontend_pl/src/Components/Navbar/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <>
-    <header className="header">
+          <header className="header">
+        <div className="header-content">
           <Link to="/" className="logo">РПЛ Статистика 23/24</Link>
-      <nav className="navbar">
-          <Link to="/players" className="nav-link">Игроки</Link> 
-          <Link to="/teams" className="nav-link">Команды</Link>
-          <Link to="/positions"className="nav-link">Позиции</Link>
-        </nav>
-    </header>
-    </>
+          <nav className="navbar">
+            <Link to="/players" className="nav-link">Игроки</Link> 
+            <Link to="/teams" className="nav-link">Команды</Link> 
+            <Link to="/positions" className="nav-link">Позиции</Link> 
+          </nav>
+        </div>
+      </header>
   );
 };
 
