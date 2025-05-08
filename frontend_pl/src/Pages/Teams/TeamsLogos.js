acd84c774821599afc7ceac66c85ed4689280574
kol4egraf0-0 <genitya408@gmail.com>
2025-05-08 12:46:32 +0300

    update teamsLogos

---
+++ frontend_pl/src/Pages/Teams/TeamsLogos.js
@@ -0,0 +1,20 @@
+const teamLogos = {
+  "Zenit": "https://img.championat.com/team/logo/1688043161757065608.png", //ок
+  "Spartak Moscow": "https://img.championat.com/team/logo/16523023911931541032.png",
+  "CSKA Moscow": "https://img.championat.com/team/logo/1465837941322079237.png",
+  "Krasnodar": "https://img.championat.com/team/logo/14676324111013772033.png",
+  "Loko Moscow": "https://img.championat.com/team/logo/14658384771344549206.png",
+  "Dynamo Mosc": "https://img.championat.com/team/logo/1465838729886051809.png",
+  "Rubin Kazan": "https://img.championat.com/team/logo/15619832822012126038.png",
+  "Rostov": "https://img.championat.com/team/logo/14658384351941932061.png",
+  "Akhmat Grozny": "https://img.championat.com/team/logo/1501229909577937100.png",
+  "Orenburg": "https://img.championat.com/team/logo/1468673791752765441.png",
+  "Fakel Voronezh": "https://img.championat.com/team/logo/16770706191821416934.png",
+  "Sochi": "https://img.championat.com/team/logo/1531215018203072749.png",
+  "FC Baltika Kaliningrad": "https://img.championat.com/team/logo/1689198863840844941.png",
+  "Ural": "https://img.championat.com/team/logo/14658380112046023866.png",
+  "Nizhny Novgorod": "https://img.championat.com/team/logo/16999643801374223479.png",
+  "Samara": "https://img.championat.com/team/logo/1465838512157254062.png",
+};
+
+export default teamLogos;
