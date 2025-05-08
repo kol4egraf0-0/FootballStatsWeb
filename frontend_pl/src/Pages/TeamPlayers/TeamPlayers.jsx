89fe4067e25fe92e08a353c3ea103c1c5d05adfe
kol4egraf0-0 <genitya408@gmail.com>
2025-05-08 16:43:13 +0300

    add css

--- frontend_pl/src/Pages/TeamPlayers/TeamPlayers.jsx
+++ frontend_pl/src/Pages/TeamPlayers/TeamPlayers.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
-
+import "./TeamPlayers.css"
 
 function TeamPlayers() {
   const [players, setPlayers] = useState([]);
