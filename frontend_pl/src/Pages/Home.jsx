d7d1849d985dd5b74817b8e6a0ad2a1abd59e307
kol4egraf0-0 <genitya408@gmail.com>
2025-05-08 10:16:49 +0300

    add home full pack

--- frontend_pl/src/Pages/Home.jsx
+++ frontend_pl/src/Pages/Home.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div className="page">
-      <h1>РПЛ 23/24</h1>
-      <p>Здесь будет таблица с игроками...</p>
+    <div className="home-container">
+      <header className="hero-section">
+        <h1>🏆</h1>
+        <p>Платформа о футболистах, командах и их статистике в сезоне 2023/24.</p>
+      </header>
+
+      <section className="cards-section">
+        <div className="info-card">
+          <h2>👥 Игроки</h2>
+          <p>Полные списки футболистов всех команд, профили, позиции и возраст.</p>
+        </div>
+
+        <div className="info-card">
+          <h2>🏟️ Команды</h2>
+          <p>Информация о клубах.</p>
+        </div>
+
+        <div className="info-card">
+          <h2>📊 Статистика</h2>
+          <p>Вся стата в одном месте.</p>
+        </div>
+      </section>
+
+      <footer className="footer">
+        <p>Сделано мной :D</p>
+      </footer>
     </div>
   );
 }
