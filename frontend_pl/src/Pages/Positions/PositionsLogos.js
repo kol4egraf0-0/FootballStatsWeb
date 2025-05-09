2cee877ec76ec5655199d1f8acc1281e5dce9455
kol4egraf0-0 <genitya408@gmail.com>
2025-05-09 12:11:29 +0300

    add fotos+update css

--- frontend_pl/src/Pages/Positions/PositionsLogos.js
+++ frontend_pl/src/Pages/Positions/PositionsLogos.js
@@ -1,5 +1,14 @@
 const positionLogos = {
-        "DF": "",
+        "DF": "https://avatars.mds.yandex.net/get-entity_search/5396253/1153407984/S600xU_2x",
+        "DF,FW": "https://avatars.mds.yandex.net/get-entity_search/5456976/922725987/S600xU_2x",
+        "DF,MF":"https://avatars.mds.yandex.net/get-entity_search/1963038/1153103239/S600xU_2x",
+        "FW": "https://avatars.mds.yandex.net/i?id=4b04330337dd211c4d4badd988ce9685_l-5865537-images-thumbs&n=13",
+        "FW,DF": "https://avatars.mds.yandex.net/get-entity_search/1870652/1155542368/S600xU_2x",
+        "FW,MF":"https://cdn.iz.ru/sites/default/files/Content/media/3/news/2014/11/579808/66b37265005a042e062d6540d3bf239e.jpg",
+        "GK":"https://avatars.mds.yandex.net/get-entity_search/2115007/1131625288/S600xU_2x",
+        "MF":"https://avatars.mds.yandex.net/get-entity_search/2480709/990639370/S600xU_2x",
+        "MF,DF":"https://avatars.mds.yandex.net/get-entity_search/9710637/793553650/S600xU_2x",
+        "MF,FW":"https://avatars.mds.yandex.net/get-entity_search/2032283/1155289538/S600xU_2x"
 };
 
 export default positionLogos;
